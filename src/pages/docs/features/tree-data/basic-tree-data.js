26f575a33ce25452ee9a86244c5d2c4653c26df3
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2019-04-23 11:07:44 +0300

    tree-data page

---
+++ src/pages/docs/features/tree-data/basic-tree-data.js
@@ -0,0 +1,85 @@
+class BasicTreeData extends React.Component {
+  render() {
+    return (
+      <MaterialTable
+        title="Basic Tree Data Preview"
+        data={[
+          {
+            id: 1,
+            name: 'a',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 63,
+            sex: 'Male',
+            type: 'adult',
+          },
+          {
+            id: 2,
+            name: 'b',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 34,
+            sex: 'Female',
+            type: 'adult',
+            parentId: 1,
+          },
+          {
+            id: 3,
+            name: 'c',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 34,
+            sex: 'Female',
+            type: 'child',
+            parentId: 1,
+          },
+          {
+            id: 4,
+            name: 'd',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 34,
+            sex: 'Female',
+            type: 'child',
+            parentId: 3,
+          },
+          {
+            id: 5,
+            name: 'e',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 34,
+            sex: 'Female',
+            type: 'child',
+          },
+          {
+            id: 6,
+            name: 'f',
+            surname: 'Baran',
+            birthYear: 1987,
+            birthCity: 34,
+            sex: 'Female',
+            type: 'child',
+            parentId: 5,
+          },
+        ]}
+        columns={[
+          { title: 'Adı', field: 'name' },
+          { title: 'Soyadı', field: 'surname' },
+          { title: 'Cinsiyet', field: 'sex' },
+          { title: 'Tipi', field: 'type', removable: false },
+          { title: 'Doğum Yılı', field: 'birthYear', type: 'numeric' },
+          {
+            title: 'Doğum Yeri',
+            field: 'birthCity',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+          },
+        ]}
+        parentChildData={(row, rows) => rows.find(a => a.id === row.parentId)}
+        options={{
+          selection: true,
+        }}
+      />
+    )
+  }
+}
