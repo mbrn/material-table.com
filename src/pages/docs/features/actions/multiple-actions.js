dfbeeb87a2da37dcbb5571e88ae63ea96aee9e0a
AugustoCalaca <augustocalaca2@gmail.com>
2020-01-09 23:12:47 -0300

    update actions examples

--- src/pages/docs/features/actions/multiple-actions.js
+++ src/pages/docs/features/actions/multiple-actions.js
@@ -1,35 +1,33 @@
-class MultipleActions extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        title="Multiple Actions Preview"
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}        
-        actions={[
-          {
-            icon: 'save',
-            tooltip: 'Save User',
-            onClick: (event, rowData) => alert("You saved " + rowData.name)
-          },
-          {
-            icon: 'delete',
-            tooltip: 'Delete User',
-            onClick: (event, rowData) => confirm("You want to delete " + rowData.name)
-          }
-        ]}
-      />
-    )
-  }
+function MultipleActions() {
+  return (
+    <MaterialTable
+      title="Multiple Actions Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}        
+      actions={[
+        {
+          icon: 'save',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => alert("You saved " + rowData.name)
+        },
+        {
+          icon: 'delete',
+          tooltip: 'Delete User',
+          onClick: (event, rowData) => confirm("You want to delete " + rowData.name)
+        }
+      ]}
+    />
+  )
 }
--- src/pages/docs/features/actions/positioning-actions-column.js
+++ src/pages/docs/features/actions/positioning-actions-column.js
@@ -1,39 +1,37 @@
-class PositioningActionsColumn extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        title="Positioning Actions Column Preview"
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}
-        actions={[
-          {
-            icon: 'save',
-            tooltip: 'Save User',
-            onClick: (event, rowData) => alert("You saved " + rowData.name)
-          },
-          rowData => ({
-            icon: 'delete',
-            tooltip: 'Delete User',
-            onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
-            disabled: rowData.birthYear < 2000
-          })
-        ]}
-        options={{
-          actionsColumnIndex: -1
-        }}
-      />
-    )
-  }
+function PositioningActionsColumn() {
+  return (
+    <MaterialTable
+      title="Positioning Actions Column Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}
+      actions={[
+        {
+          icon: 'save',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => alert("You saved " + rowData.name)
+        },
+        rowData => ({
+          icon: 'delete',
+          tooltip: 'Delete User',
+          onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
+          disabled: rowData.birthYear < 2000
+        })
+      ]}
+      options={{
+        actionsColumnIndex: -1
+      }}
+    />
+  )
 }
