e218800aa5f7019363d9e0ee2c825a025860867f
AugustoCalaca <augustocalaca2@gmail.com>
2020-01-09 23:32:09 -0300

    update detail-panel example

--- src/pages/docs/features/detail-panel/detail-panel-with-rowclick.js
+++ src/pages/docs/features/detail-panel/detail-panel-with-rowclick.js
@@ -1,36 +1,34 @@
-class DetailPanelWithRowClick extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-        ]}
-        title="Detail Panel With RowClick Preview"
-        detailPanel={rowData => {
-          return (
-            <iframe
-              width="100%"
-              height="315"
-              src="https://www.youtube.com/embed/C0DPdy98e4c"
-              frameborder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
-            />
-          )
-        }}
-        onRowClick={(event, rowData, togglePanel) => togglePanel()}
-      />
-    )
-  }
+function DetailPanelWithRowClick() {
+  return (
+    <MaterialTable
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+      ]}
+      title="Detail Panel With RowClick Preview"
+      detailPanel={rowData => {
+        return (
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/C0DPdy98e4c"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          />
+        )
+      }}
+      onRowClick={(event, rowData, togglePanel) => togglePanel()}
+    />
+  )
 }
--- src/pages/docs/features/detail-panel/multiple-detail-panels.js
+++ src/pages/docs/features/detail-panel/multiple-detail-panels.js
@@ -1,79 +1,77 @@
-class MultipleDetailPanels extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        title="Multiple Detail Panels Preview"
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+function MultipleDetailPanels() {
+  return (
+    <MaterialTable
+      title="Multiple Detail Panels Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}
+      detailPanel={[
+        {
+          tooltip: 'Show Name',
+          render: rowData => {
+            return (
+              <div
+                style={{
+                  fontSize: 100,
+                  textAlign: 'center',
+                  color: 'white',
+                  backgroundColor: '#43A047',
+                }}
+              >
+                {rowData.name}
+              </div>
+            )
           },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}
-        detailPanel={[
-          {
-            tooltip: 'Show Name',
-            render: rowData => {
-              return (
-                <div
-                  style={{
-                    fontSize: 100,
-                    textAlign: 'center',
-                    color: 'white',
-                    backgroundColor: '#43A047',
-                  }}
-                >
-                  {rowData.name}
-                </div>
-              )
-            },
+        },
+        {
+          icon: 'account_circle',
+          tooltip: 'Show Surname',
+          render: rowData => {
+            return (
+              <div
+                style={{
+                  fontSize: 100,
+                  textAlign: 'center',
+                  color: 'white',
+                  backgroundColor: '#E53935',
+                }}
+              >
+                {rowData.surname}
+              </div>
+            )
           },
-          {
-            icon: 'account_circle',
-            tooltip: 'Show Surname',
-            render: rowData => {
-              return (
-                <div
-                  style={{
-                    fontSize: 100,
-                    textAlign: 'center',
-                    color: 'white',
-                    backgroundColor: '#E53935',
-                  }}
-                >
-                  {rowData.surname}
-                </div>
-              )
-            },
+        },
+        {
+          icon: 'favorite_border',
+          openIcon: 'favorite',
+          tooltip: 'Show Both',
+          render: rowData => {
+            return (
+              <div
+                style={{
+                  fontSize: 100,
+                  textAlign: 'center',
+                  color: 'white',
+                  backgroundColor: '#FDD835',
+                }}
+              >
+                {rowData.name} {rowData.surname}
+              </div>
+            )
           },
-          {
-            icon: 'favorite_border',
-            openIcon: 'favorite',
-            tooltip: 'Show Both',
-            render: rowData => {
-              return (
-                <div
-                  style={{
-                    fontSize: 100,
-                    textAlign: 'center',
-                    color: 'white',
-                    backgroundColor: '#FDD835',
-                  }}
-                >
-                  {rowData.name} {rowData.surname}
-                </div>
-              )
-            },
-          },
-        ]}
-      />
-    )
-  }
+        },
+      ]}
+    />
+  )
 }
