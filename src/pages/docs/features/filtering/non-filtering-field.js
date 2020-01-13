a5328187144de02ea5fb1d80c3975dfa679bf6bc
AugustoCalaca <augustocalaca2@gmail.com>
2020-01-12 22:55:51 -0300

    update filtering examples

--- src/pages/docs/features/filtering/non-filtering-field.js
+++ src/pages/docs/features/filtering/non-filtering-field.js
@@ -1,26 +1,24 @@
-class NonFilteringField extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        title="Non Filtering Field Preview"
-        columns={[
-          { title: 'Name', field: 'name', filtering: false },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}        
-        options={{
-          filtering: true
-        }}
-      />
-    )
-  }
+function NonFilteringField() {
+  return (
+    <MaterialTable
+      title="Non Filtering Field Preview"
+      columns={[
+        { title: 'Name', field: 'name', filtering: false },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}        
+      options={{
+        filtering: true
+      }}
+    />
+  )
 }
