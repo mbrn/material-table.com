8075cabb3a26bf979bb98d490f6cc37ccbff5372
AugustoCalaca <augustocalaca2@gmail.com>
2020-01-13 23:35:43 -0300

    update selection examples

--- src/pages/docs/features/selection/actions-on-selected-rows.js
+++ src/pages/docs/features/selection/actions-on-selected-rows.js
@@ -1,33 +1,31 @@
-class ActionsOnSelectedRows extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        title="Actions On Selected Rows Preview"
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}        
-        options={{
-          selection: true
-        }}
-        actions={[
-          {
-            tooltip: 'Remove All Selected Users',
-            icon: 'delete',
-            onClick: (evt, data) => alert('You want to delete ' + data.length + ' rows')
-          }
-        ]}
-      />
-    )
-  }
+function ActionsOnSelectedRows() {
+  return (
+    <MaterialTable
+      title="Actions On Selected Rows Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}        
+      options={{
+        selection: true
+      }}
+      actions={[
+        {
+          tooltip: 'Remove All Selected Users',
+          icon: 'delete',
+          onClick: (evt, data) => alert('You want to delete ' + data.length + ' rows')
+        }
+      ]}
+    />
+  )
 }
--- src/pages/docs/features/selection/handling-selection-changes.js
+++ src/pages/docs/features/selection/handling-selection-changes.js
@@ -1,27 +1,25 @@
-class HandlingSelectionChanges extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        title="Handling Selection Changes Preview"
-        columns={[
-          { title: 'Name', field: 'name' },
-          { title: 'Surname', field: 'surname' },
-          { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-          {
-            title: 'Birth Place',
-            field: 'birthCity',
-            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-          },
-        ]}
-        data={[
-          { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-          { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-        ]}
-        options={{
-          selection: true
-        }}
-        onSelectionChange={(rows) => alert('You selected ' + rows.length + ' rows')}
-      />
-    )
-  }
+function HandlingSelectionChanges() {
+  return (
+    <MaterialTable
+      title="Handling Selection Changes Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}
+      options={{
+        selection: true
+      }}
+      onSelectionChange={(rows) => alert('You selected ' + rows.length + ' rows')}
+    />
+  )
 }
